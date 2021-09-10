5615aa394ec2c218c8d66ebf689a5842ea7d2fc2
rsaini777 <46646514+rsaini777@users.noreply.github.com>
2021-09-10 15:04:03 +0530

    first commit

--- server/model/upload.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const UploadSchema = new mongoose.Schema(
-  {
-    image: String
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("Upload", UploadSchema);
